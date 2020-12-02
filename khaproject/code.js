855541fd2fbdd3bcb3ca9539984c95c57624c7c8
Namira Guirous <guir0012@algonquinlive.com>
2020-12-01 20:30:58 -0500

    images

--- khaproject/code.js
+++ khaproject/code.js
@@ -1,14 +1,14 @@
 /*function to change banner*/
-var text = [ "Chrismas offers Limited time offers", "Welcome Gift ...","Free shipping for orders over 100$"];
+var text = ["Chrismas offers Limited time offers", "Welcome Gift ...", "Free shipping for orders over 100$"];
 var counter = 0;
 var inst = setInterval(change, 4000);
 
 function change() {
-  document.getElementById("changeBanner").innerHTML = text[counter];
-  counter++;
-  if (counter >= text.length) {
-    counter = 0;
-  }
+    document.getElementById("changeBanner").innerHTML = text[counter];
+    counter++;
+    if (counter >= text.length) {
+        counter = 0;
+    }
 }
 
 /* function for header slideshow */
@@ -16,22 +16,22 @@ var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+    showSlides(slideIndex += n);
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+    showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  slides[slideIndex-1].style.display = "block";  
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
 }
 
 /* change mode light/dark */
@@ -40,21 +40,20 @@ function changemode(mode) {
 
     if (mode == light) {
         document.getElementsByTagName("body")[0].style.backgroundColor = "white";
-    }
-    else{
+    } else {
         document.getElementsByTagName("body")[0].style.backgroundColor = "black";
     }
-  }
+}
 
-  /*chat window */
+/*chat window */
 
-  function openForm() {
+function openForm() {
     document.getElementById("myForm").style.display = "block";
     document.getElementById("open-button").style.display = "none";
 
-  }
+}
 
-  function closeForm() {
+function closeForm() {
     document.getElementById("open-button").style.display = "block";
     document.getElementById("myForm").style.display = "none";
-  }
+}
