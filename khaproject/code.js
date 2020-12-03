1e308eb5d8b6e9fbdc30b8f7af662171deccac1e
unknown <akha0005@algonquinlive.com>
2020-12-02 22:58:47 -0500

    js

--- khaproject/code.js
+++ khaproject/code.js
@@ -1,14 +1,14 @@
 /*function to change banner*/
-var text = ["Chrismas offers Limited time offers", "Welcome Gift ...", "Free shipping for orders over 100$"];
+var text = [ "BIG XMAS SALE -50% OFF - LIMITED OFFER", "DUE TO THE COVID-19 OUR RETURN POLICY HAS CHANGED","FREE SHIPPING ON ALL ORDERS OVER 100$"];
 var counter = 0;
 var inst = setInterval(change, 4000);
 
 function change() {
-    document.getElementById("changeBanner").innerHTML = text[counter];
-    counter++;
-    if (counter >= text.length) {
-        counter = 0;
-    }
+  document.getElementById("changeBanner").innerHTML = text[counter];
+  counter++;
+  if (counter >= text.length) {
+    counter = 0;
+  }
 }
 
 /* function for header slideshow */
@@ -16,22 +16,22 @@ var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-    showSlides(slideIndex += n);
+  showSlides(slideIndex += n);
 }
 
 function currentSlide(n) {
-    showSlides(slideIndex = n);
+  showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex - 1].style.display = "block";
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "block";  
 }
 
 /* change mode light/dark */
@@ -40,53 +40,23 @@ function changemode(mode) {
 
     if (mode == light) {
         document.getElementsByTagName("body")[0].style.backgroundColor = "white";
-    } else {
-        document.getElementsByTagName("body")[0].style.backgroundColor = "black";
+        document.getElementsByTagName("body")[0].style.Color = "black";
     }
-}
+    else{
+        document.getElementsByTagName("body")[0].style.backgroundColor = "black";
+        document.getElementsByTagName("body")[0].style.color = "white";
+    }
+  }
 
-/*chat window */
+  /*chat window */
 
-function openForm() {
+  function openForm() {
     document.getElementById("myForm").style.display = "block";
     document.getElementById("open-button").style.display = "none";
 
-}
+  }
 
-function closeForm() {
+  function closeForm() {
     document.getElementById("open-button").style.display = "block";
     document.getElementById("myForm").style.display = "none";
-}
-
-/*cart code */
-
-
-function calcSum() {
-    var total = 0;
-    var subTotal = 0;
-    var taxVal = 0;
-    var TAX = 0.08
-    for (i = 1; i < 3; i++) {
-        q = document.getElementById("q-" + i);
-        p = document.getElementById("p-" + i);
-        qty = q.value;
-        price = parseInt(p.innerText.replace('$', ''));
-        console.log(price);
-        subTotal += qty * price;
-    }
-    console.log(qty);
-
-    sub = document.getElementById("sub");
-    tot = document.getElementById("tot");
-    tax = document.getElementById("tx");
-    taxVal = subTotal * TAX;
-    tax.innerText = taxVal;
-    total = subTotal + taxVal;
-    sub.innerText = subTotal;
-    tot.innerText = total;
-}
-
-function inputClick(e) {
-    console.log(e.value);
-    calcSum();
-}
+  }
